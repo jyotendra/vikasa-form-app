4b2e2ed7e96278a74427191a80f2379602cfc101
Jyotendra Sharma <jyotendra@govividly.com>
2025-03-15 16:24:38 +0530

    added dbClient plugin check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,6 +32,7 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
+
   void fastify.register(AutoLoad, {
     dir: join(__dirname, "plugins"),
     options: opts,
--- backend/src/plugins/dynamodb.plugin.ts
+++ backend/src/plugins/dynamodb.plugin.ts
@@ -1,30 +1,29 @@
-import fp from 'fastify-plugin';
-import { FastifyPluginAsync } from 'fastify';
-import { createDynamoDBClient } from '../config/dynamodb.config';
+import fp from "fastify-plugin";
+import { FastifyPluginAsync } from "fastify";
+import { createDynamoDBClient } from "../config/dynamodb.config";
+import { AppOptions } from "../app";
 
-interface DynamoDBPluginOptions {
-  region: string;
-  endpoint?: string;
-}
-
-const dynamoDBPlugin: FastifyPluginAsync<DynamoDBPluginOptions> = async (fastify, options) => {
+const dynamoDBPlugin: FastifyPluginAsync<AppOptions> = async (
+  fastify,
+  options
+) => {
   const docClient = createDynamoDBClient({
-    region: options.region,
-    endpoint: options.endpoint,
+    region: options.aws.region,
+    endpoint: options.aws.endpoint,
   });
 
-  fastify.decorate('dynamodb', docClient);
+  fastify.decorate("dynamodb", docClient);
 
-  fastify.addHook('onClose', (instance, done) => {
+  fastify.addHook("onClose", (instance, done) => {
     done();
   });
 };
 
 export default fp(dynamoDBPlugin, {
-  name: 'dynamodb',
+  name: "dynamodb",
 });
 
-declare module 'fastify' {
+declare module "fastify" {
   interface FastifyInstance {
     dynamodb: ReturnType<typeof createDynamoDBClient>;
   }
