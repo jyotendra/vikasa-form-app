36d5140d0fc8a21f25d35d809aa647a03472f1cd
Jyotendra Sharma <jyotendra@govividly.com>
2025-03-15 19:47:21 +0530

    simplified test

--- backend/test/plugins/dynamodb-config.test.ts
+++ backend/test/plugins/dynamodb-config.test.ts
@@ -1,15 +1,10 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { build } from "fastify-cli/helper";
+import { build } from "../helper";
 
 describe("app should have dbClient", () => {
   it("should be a function", async (t) => {
-    const builtEntryFile = ["dist/app.js"];
-
-    const app = await build(builtEntryFile, {
-      skipOverride: true, // important since we want the application to be registered with fastify-plugin
-    });
+    const app = await build(t);
     assert.ok(app.dynamodb);
-    t.after(() => app.close());
   });
 });
